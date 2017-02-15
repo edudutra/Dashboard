a93e7af8cff6684c7b7ab40ca9c999ba5d019172
Eduardo Dutra <emdutra@gmail.com>
2017-02-15 18:33:46 -0200

    Verificação inicial do módulo Dashboard

---
+++ Dashboard/Controllers/IService.cs
@@ -0,0 +1,26 @@
+﻿namespace Dashboard.Controllers
+{
+	public interface IService
+	{
+	}
+
+	public class Service : IService
+	{
+		public IContext _context
+		{
+			get;
+			private set;
+		}
+
+		public Service(IContext context)
+		{
+			_context = context;
+		}
+	}
+
+	public interface IContext { }
+
+	public class Context : IContext
+	{ }
+
+}
---
+++ Dashboard/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Dashboard.Controllers;
+using DryIoc;
+using DryIoc.Mvc;
+
+namespace Dashboard
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			var container = new Container();
+			container.Register<IService, Service>();
+			container.Register<IContext, Context>();
+
+			container.WithMvc();
+		}
+	}
+}
